a34020f06d894118994292c40868add461927596
ItimPar <parinat2@hotmail.com>
2022-09-22 12:18:23 +0700

    done texture selector

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,22 +1,26 @@
 import create from 'zustand'
 import { nanoid } from 'nanoid'
 
+const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key))
+const setLocalStorage = (key, value) => window.localStorage.setItem(key, JSON.stringify(value))
+
+
 export const useStore = create((set) => ({
-  texture: 'dirt',
-  cubes: [],
-  addCube: (x, y, z) => {
-    set((prev) => ({
-      cubes: [
-        ...prev.cubes,
-        {
-          key: nanoid(),
-          pos: [x, y, z],
-          texture: prev.texture
-        }
-      ]
-    }))
-  },
-  removeCube: (x, y, z) => {
+	texture: 'dirt',
+	cubes: getLocalStorage('cubes') || [],
+	addCube: (x, y, z) => {
+		set((prev) => ({
+			cubes: [
+				...prev.cubes,
+				{
+					key: nanoid(),
+					pos: [x, y, z],
+					texture: prev.texture
+				}
+			]
+		}))
+	},
+	removeCube: (x, y, z) => {
 		set((prev) => ({
 			cubes: prev.cubes.filter(cube => {
 				const [X, Y, Z] = cube.pos
@@ -25,7 +29,19 @@ export const useStore = create((set) => ({
 
 		}))
 	},
-  setTexture: () => { },
-  saveWorld: () => { },
-  resetWorld: () => { },
+	setTexture: (texture) => {
+		set(() => ({
+			texture
+		}))
+	},
+	saveWorld: () => {
+		set((prev) => {
+			setLocalStorage('cubes', prev.cubes)
+		})
+	},
+	resetWorld: () => {
+		set(() => ({
+			cubes: []
+		}))
+	},
 }))
