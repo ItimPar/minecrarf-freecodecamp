d2acf817ee7cdc086cb3842ad3ed65f5295d2c75
ItimPar <parinat2@hotmail.com>
2022-09-22 12:33:08 +0700

    done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Player } from "./components/Player";
 import { FPV } from "./components/FPV";
 import { Cubes } from "./components/Cubes";
 import { TextureSelector } from "./components/TextureSelector";
-
+import { Menu } from "./components/Menu"
 
 
 function App() {
@@ -25,6 +25,7 @@ function App() {
       </Canvas>
       <div className="absolute centered cursor">+</div>
       <TextureSelector />
+      <Menu />
     </>
   );
 }
